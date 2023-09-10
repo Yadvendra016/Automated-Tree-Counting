0a4195cfe020fc3bbf87818d0e116df7e9d139c1
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2023-09-10 10:58:53 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <h1>Hey how are you</h1>
-    </div>
+      <Footer />
+    </>
   );
 }
 
